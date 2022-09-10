e5da6d004443c9244f33f72216c2f0f3e34352a6
Gabriel Moura <40903360+gmm9@users.noreply.github.com>
2022-09-10 17:18:13 -0300

    Add files via upload

---
+++ reconhecimentoDeVoz.js
@@ -0,0 +1,41 @@
+const elementoChute = document.querySelector(".container");
+let i = 1;
+window.SpeechRecognition = window.SpeechRecongnition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.lang = "pt-Br";
+recognition.start();
+
+recognition.addEventListener("result", onSpeak);
+
+function onSpeak(e) {
+  chute = e.results[0][0].transcript;
+  if (liberarGame) {
+    console.log("Voice liberada");
+    exibeChuteNaTela(chute);
+    if(i < rounds) {
+        verificaSeOChutePossuiUmValorValido(chute);
+        // i++;
+    }
+    else {
+        document.body.innerHTML = `
+        <h2>Você Errou!</h2>
+        <h2>GAME OVER</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+        document.body.style.background = 'red';
+    }
+  } else {
+    console.log("Voz não liberada");
+  }
+}
+function exibeChuteNaTela(chute) {
+  elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+    `;
+}
+recognition.addEventListener("end", () => recognition.start());
